5a96c50b98ed6faeb9660793a5329b2c6d9fc6cd
Steven Fackler <sfackler@gmail.com>
2015-02-08 06:39:06 +0000

    rebuild pages at ca0a89dbc5b76a931190b730f76062ab4c030ac8

---
+++ doc/implementors/postgres/types/trait.RawToSql.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres_range'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;T&gt; <a class='trait' href='https://sfackler.github.io/doc/postgres/types/trait.RawToSql.html' title='postgres::types::RawToSql'>RawToSql</a> for <a class='struct' href='postgres_range/struct.Range.html' title='postgres_range::Range'>Range</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
