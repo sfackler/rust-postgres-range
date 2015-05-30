997b1cfeaa93d05f8a484d07354271da7c5c692d
Steven Fackler <sfackler@gmail.com>
2015-05-30 12:27:29 -0700

    Release v0.7.2

---
+++ doc/v0.7.2/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres_range'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;S, T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='postgres_range/struct.RangeBound.html' title='postgres_range::RangeBound'>RangeBound</a>&lt;S, T&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='postgres_range/struct.Range.html' title='postgres_range::Range'>Range</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
