0f3c36fac55392be11f0806a56ca087b27addc28
Steven Fackler <sfackler@gmail.com>
2015-09-19 12:38:48 -0700

    Release v0.7.3

---
+++ doc/v0.7.3/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres_range'] = ["impl&lt;S, T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='postgres_range/struct.RangeBound.html' title='postgres_range::RangeBound'>RangeBound</a>&lt;S, T&gt; <span class='where'>where S: <a class='trait' href='postgres_range/trait.BoundSided.html' title='postgres_range::BoundSided'>BoundSided</a>, T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a></span>","impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='postgres_range/struct.Range.html' title='postgres_range::Range'>Range</a>&lt;T&gt; <span class='where'>where T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a></span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/v0.7.3/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres_range'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='postgres_range/enum.BoundSide.html' title='postgres_range::BoundSide'>BoundSide</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='postgres_range/enum.BoundType.html' title='postgres_range::BoundType'>BoundType</a>","impl&lt;S, T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='postgres_range/struct.RangeBound.html' title='postgres_range::RangeBound'>RangeBound</a>&lt;S, T&gt; <span class='where'>where S: <a class='trait' href='postgres_range/trait.BoundSided.html' title='postgres_range::BoundSided'>BoundSided</a>, T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a></span>","impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='postgres_range/struct.Range.html' title='postgres_range::Range'>Range</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
