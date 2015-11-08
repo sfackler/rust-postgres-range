053992c918a245448ba93b1d58fb7a738e8e6deb
Steven Fackler <sfackler@gmail.com>
2015-11-07 17:12:12 -0800

    Release v0.7.4

---
+++ doc/v0.7.4/implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres_range'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='postgres_range/enum.BoundSide.html' title='postgres_range::BoundSide'>BoundSide</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='postgres_range/enum.BoundType.html' title='postgres_range::BoundType'>BoundType</a>","impl&lt;S, T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='postgres_range/struct.RangeBound.html' title='postgres_range::RangeBound'>RangeBound</a>&lt;S, T&gt; <span class='where'>where S: <a class='trait' href='postgres_range/trait.BoundSided.html' title='postgres_range::BoundSided'>BoundSided</a>, T: <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a></span>","impl&lt;T: <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='postgres_range/struct.Range.html' title='postgres_range::Range'>Range</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/v0.7.4/implementors/postgres_range/trait.BoundSided.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres_range'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
