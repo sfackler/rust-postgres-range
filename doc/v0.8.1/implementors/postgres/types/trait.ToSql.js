569b531606fb3a40d73a6282ddbdcac2caef292c
Steven Fackler <sfackler@palantir.com>
2016-01-22 10:04:59 -0800

    Release v0.8.1

---
+++ doc/v0.8.1/implementors/postgres/types/trait.ToSql.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres_range'] = ["impl&lt;T&gt; <a class='trait' href='https://sfackler.github.io/rust-postgres/doc/v0.11.0/postgres/types/trait.ToSql.html' title='postgres::types::ToSql'>ToSql</a> for <a class='struct' href='postgres_range/struct.Range.html' title='postgres_range::Range'>Range</a>&lt;T&gt; <span class='where'>where T: <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> + <a class='trait' href='postgres_range/trait.Normalizable.html' title='postgres_range::Normalizable'>Normalizable</a> + <a class='trait' href='https://sfackler.github.io/rust-postgres/doc/v0.11.0/postgres/types/trait.ToSql.html' title='postgres::types::ToSql'>ToSql</a></span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
