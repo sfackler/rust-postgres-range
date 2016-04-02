7271cdaf8ef43f04b8b70a01e9733b526c2dafca
Steven Fackler <sfackler@gmail.com>
2016-04-02 16:02:27 -0700

    Release v0.8.2

---
+++ doc/v0.8.2/implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres_range'] = ["impl&lt;S, T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='postgres_range/struct.RangeBound.html' title='postgres_range::RangeBound'>RangeBound</a>&lt;S, T&gt; <span class='where'>where S: <a class='trait' href='postgres_range/trait.BoundSided.html' title='postgres_range::BoundSided'>BoundSided</a>, T: <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a></span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
